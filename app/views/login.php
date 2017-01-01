8001a848aaa5657581cdf85090d2e7854a35db27
nimeshmora <mhnimesh@gmail.com>
2017-01-01 17:18:49 +0530

    initial commit laravel system

---
+++ app/views/login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Login - Inventory Control System</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
+
+<div class="login">
+    <form action="index.php" method="post">
+
+        <fieldset>
+            <legend><h3>Login</h3></legend>
+
+            <p>
+                <label for="">Username</label>
+                <input type="text" name="email" placeholder="Email address">
+            </p>
+
+
+            <p>
+
+                <label for="">Password</label>
+                <input type="password" name="password" placeholder="Password">
+
+            </p>
+
+            <p>
+
+                <button type="submit" name="submit">Login</button>
+            </p>
+
+        </fieldset>
+
+    </form>
+
+</div>
+
+</body>
+</html>
